1b268d6ee85a64a5228f00a0b8b92e9b881500d0
Ravi Kolla <ravi.kolla@outlook.com>
2019-09-02 14:52:08 -0400

    Google Adsense update

--- src/components/GoogleAdsense.js
+++ src/components/GoogleAdsense.js
@@ -3,14 +3,17 @@ import React, {useEffect} from 'react';
 const GoogleAdComponent = () => {
 
   useEffect(() => {
-    (window.adsbygoogle = window.adsbygoogle || []).push({});
+    (adsbygoogle = window.adsbygoogle || []).push({
+      google_ad_client: "ca-pub-8915459609879797",
+      enable_page_level_ads: true
+    });
   })
 
   return (
       <ins className='adsbygoogle'
         style={{ display: 'block' }}
-        data-ad-client='ca-pub-12121212'
-        data-ad-slot='12121212'
+        data-ad-client='ca-pub-8915459609879797'
+        data-ad-slot='8915459609879797'
         data-ad-format='auto' />
   );
 }

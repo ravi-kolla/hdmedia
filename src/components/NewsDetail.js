8057abbd9cd12064ad40ab55e82e84482e081246
Ravi Kolla <ravi.kolla@outlook.com>
2019-09-01 23:03:59 -0400

    Update to google adsense

--- src/components/NewsDetail.js
+++ src/components/NewsDetail.js
@@ -24,7 +24,6 @@ const NewsDetail = (props) => {
               <p class="card-text"><small class="text-muted">By {props.newsItem.author} - {props.newsItem.date}</small></p>
             </div>
             <img class="card-img-bottom" src={props.newsItem.image} alt="{props.newsItem.title}" />
-            <GoogleAdComponent />
             <div class="card-body">
               <p class="card-text">{props.newsItem.details}</p>
             </div>

c984e0a903f4a5c88a50c646173473baa33d8dd8
Ravi Kolla <ravi.kolla@outlook.com>
2019-09-06 00:59:19 -0400

    FestivalWish component update

--- src/components/FestivalWish.js
+++ src/components/FestivalWish.js
@@ -9,12 +9,13 @@ const FestivalWish = (props) => {
         <meta name="description" content={props.festival.title} />
       </Helmet>
       <div>
-        <img class="card-img-top festival-banner" src={props.festival.banner} alt={props.festival.title} />
-        <div class="card-body">
-          <h5 class="card-title">{props.festival.title}</h5>
-          <p class="card-text"><small class="text-muted">From {props.festival.author} - {props.festival.date}</small></p>
-          <p class="card-text">{props.festival.description}</p>
-          {props.festival.details ? <p class="card-text">{props.festival.details}</p> : null}
+        <img className="card-img-top festival-banner" src={props.festival.banner} alt={props.festival.title} />
+        <div className="card-body">
+          <h5 className="card-title">{props.festival.title}</h5>
+          <p className="card-text"><small class="text-muted">From {props.festival.author} - {props.festival.date}</small></p>
+          <p className="card-text">{props.festival.description}</p>
+          {props.festival.details ? <p className="card-text">{props.festival.details}</p> : null}
+          {props.festival.moredetails ? <p className="card-text">{props.festival.moredetails}</p> : null}
         </div>
       </div>
     </div>
--- src/content/festival.js
+++ src/content/festival.js
@@ -7,10 +7,11 @@ export const FestivalMessage = [
       banner: '/images/missionbanner.png',
       description: "Chandrayaan 2 is an Indian lunar mission that will boldly go where no country has ever gone before — the Moon's south polar region. Through this effort, the aim is to improve our understanding of the Moon — discoveries that will benefit India and humanity as a whole. These insights and experiences aim at a paradigm shift in how lunar expeditions are approached for years to come — propelling further voyages into the farthest frontiers.",
       details:"The Lander (Vikram) will do soft landing on Moon on September 7, 2019 and there after for next 14 days the Rover (Pragyan) will be exploring the moon's south polar region to under the extent of water availability and other significant things.",
+      moredetails: "Chandrayaan 2's Rover is a 6-wheeled robotic vehicle named Pragyan and this will be communicating with the Lander. Lander has the capability to communicate with IDSN at Byalalu near Bangalore, as well as with the Orbiter and Rover. Orbiter will be capable of communicating with Indian Deep Space Network (IDSN) at Byalalu as well as the Vikram Lander. The mission life of the Orbiter is one year and it will be placed in a 100X100 km lunar polar orbit.",
       author: 'Hindustan Daily Team',
       urlname: 'chandrayaan-2',
       category: 'celebrate',
-      date: 'Sep 06, 2019'
+      date: 'Sep 06, 2019. Source : ISRO'
     },
     {
         id: 1,
@@ -20,6 +21,7 @@ export const FestivalMessage = [
         banner: '/images/ganesh-chaturthi.png',
         description: 'Ganesh Chaturthi, also called Vinayaka Chavithi, is an auspicious Hindu festival which is celebrated for 10 days every year. The festival is celebrated in the Bhadra month as per the Hindu calendar which generally falls in mid-August to September. It marks the birthday of the beloved elephant-headed Lord Ganesha.',
         details:"",
+        moredetails: "",
         author: 'Hindustan Daily Team',
         urlname: 'vinaya-chaturthi',
         category: 'celebrate',
